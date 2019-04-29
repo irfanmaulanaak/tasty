a0eb0a0a0e3d40eef9fe091370beaacde564aba2
irfanmaulanaak <irfanjamesway@yahoo.co.id>
2019-04-30 00:16:45 +0700

    asfaskf

---
+++ mvc/controllers/Post.class.php
@@ -0,0 +1,13 @@
+<?php class Post extends Controller {
+
+    public function index(){
+        //Load model
+        $postModel = $this->loadModel('PostModel');
+
+        // Get data from the model
+        $posts = $postModel->getAll();
+
+        // Load the view
+        $this->loadView('posts', ['posts'=>$posts]);
+    }
+}
---
+++ mvc/models/Model.class.php
@@ -0,0 +1,16 @@
+<?php class Model {
+    protected $mysqli;
+
+    public function __construct(){
+        $dbhostname = 'localhost';
+        $dbusername = 'root';
+        $dbpassword = '';
+        $dbname = 'test';
+
+        $this->mysqli = new mysqli($dbhostname, $dbusername, $dbpassword, $dbname);
+
+        if($this->mysqli->connect_errno){
+            die('Database connection error!');
+        }
+    }
+}
