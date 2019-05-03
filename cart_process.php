68021cecfb698f90b3dc436b25778fd91d5b2f7b
irfanmaulanaak <irfanjamesway@yahoo.co.id>
2019-05-03 18:54:31 +0700

    Cart beres tinggal ui

--- cart_process.php
+++ cart_process.php
@@ -10,9 +10,16 @@ $fotoproduk = $_GET['fotoprod'];
 $uname = $_GET['username'];
 
 $sql = "INSERT INTO cart (nama_produk, harga_produk, foto_produk, username) VALUES ('$namaproduk', $hargaproduk, '$fotoproduk', '$uname')";
-echo($namaproduk);
 $mysqli->query($sql);
 
-echo '<script type="text/javascript">alert("Pesanan '.$namaproduk.' telah berhasil ditambahkan kedalam cart!");</script>';
-sleep(1);
-// header("location:javascript://history.go(-1)");
+echo "<script language='javascript' type='text/javascript'>";
+echo "confirm(\"Pesanan '.$namaproduk.' telah berhasil ditambahkan kedalam cart!\");";
+echo "window.history.back();";
+echo "</script>";
+
+// header("location:javascript://history.back(confirm(\"Pesanan '.$namaproduk.' telah berhasil ditambahkan kedalam cart!\"))");
+
+// // echo '<script type="text/javascript">alert("Pesanan '.$namaproduk.' telah berhasil ditambahkan kedalam cart!");</script>';
+// // sleep(1);
+// header("location:javascript://history.back(alert(\"Pesanan '.$namaproduk.' telah berhasil ditambahkan kedalam cart!\"))");
+
